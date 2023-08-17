682975bf237e357b2e2cb54d9adf891c325cebe4
davidholo2 <davidholo2@gmail.com>
2023-08-18 01:39:38 +0300

    finished with out deploy

---
+++ client/src/AccountNav.jsx
@@ -0,0 +1,65 @@
+import { Link, Navigate, useLocation, useParams } from "react-router-dom";
+
+export default function AccountNav() {
+  const { pathname } = useLocation();
+  let subpage = pathname.split("/")?.[2];
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+  function linkClasses(type = null) {
+    let classes = "inline-flex gap-1 py-2 px-6 rounded-full";
+    if (type === subpage) {
+      classes += " bg-primary text-white";
+    } else {
+      classes += " bg-gray-200";
+    }
+    return classes;
+  }
+
+  return (
+    <nav className="w-full flex mt-8 gap-2 justify-center mb-8">
+      <Link className={linkClasses("profile")} to={"/account"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        My Profile
+      </Link>
+      <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2.625 6.75a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0A.75.75 0 018.25 6h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75zM2.625 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 12a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12A.75.75 0 017.5 12zm-4.875 5.25a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        My Bookings
+      </Link>
+      <Link className={linkClasses("places")} to={"/account/places"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6"
+        >
+          <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+          <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+        </svg>
+        My Accommodations
+      </Link>
+    </nav>
+  );
+}
---
+++ client/src/pages/BookingsPage.jsx
@@ -0,0 +1,104 @@
+import AccountNav from "../AccountNav";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { differenceInCalendarDays, format } from "date-fns";
+import { Link } from "react-router-dom";
+import PlaceImg from "../PlaceImg";
+
+export default function BookingsPage() {
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    axios.get("/bookings").then((response) => {
+      setBookings(response.data);
+    });
+  }, []);
+  return (
+    <div>
+      <AccountNav />
+      <div>
+        {bookings?.length > 0 &&
+          bookings.map((booking) => (
+            <div className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden">
+              <div className="w-48">
+                <PlaceImg place={booking.place} />
+              </div>
+              <div className="py-3 grow pr-3">
+                <h2 className="text-xl">{booking.place.title}</h2>
+                <div className="border-t border-gray-300 flex gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+                    />
+                  </svg>
+                  {format(new Date(booking.checkIn), "dd-MM-yyyy")} &rarr;
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+                    />
+                  </svg>
+                  {format(new Date(booking.checkOut), "dd-MM-yyyy")}
+                </div>
+                <div className="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                    />
+                  </svg>
+                  Numbers of nights:
+                  {differenceInCalendarDays(
+                    new Date(booking.checkOut),
+                    new Date(booking.checkIn)
+                  )}
+                </div>
+                <div className="flex  font-bold">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                    />
+                  </svg>
+                  Total price:${booking.price}
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
